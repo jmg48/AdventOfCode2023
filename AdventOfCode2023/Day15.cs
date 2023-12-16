688144827341627e08e25874bdff945ba8a59bf8
Jon Glass <jglass@blackfog.com>
2023-12-16 08:09:32 +0000

    Day 16 Part 2

--- AdventOfCode2023/Day15.cs
+++ AdventOfCode2023/Day15.cs
@@ -1,6 +1,5 @@
 using System.Text;
 using System.Text.RegularExpressions;
-using Newtonsoft.Json.Serialization;
 
 namespace AdventOfCode2023
 {
--- AdventOfCode2023/Day16.cs
+++ AdventOfCode2023/Day16.cs
@@ -1,8 +1,3 @@
-using System.Text;
-using System.Text.RegularExpressions;
-using Newtonsoft.Json.Serialization;
-using NUnit.Framework.Constraints;
-
 namespace AdventOfCode2023
 {
     public class Day16
@@ -11,17 +6,46 @@ namespace AdventOfCode2023
         public void Part1()
         {
             var input = File.ReadLines("Input16.txt").ToList();
+            Console.WriteLine(Energy(input, (new Coord(0, -1), Dir.E)));
+        }
 
+        [Test]
+        public void Part2()
+        {
+            var input = File.ReadLines("Input16.txt").ToList();
+
+            var result = 0;
+            for (var i = 0; i < input.Count; i++)
+            {
+                var energy1 = Energy(input, (new Coord(i, -1), Dir.E));
+                var energy2 = Energy(input, (new Coord(i, input[0].Length), Dir.W));
+
+                result = Math.Max(result, Math.Max(energy1, energy2));
+            }
+
+            for (var j = 0; j < input[0].Length; j++)
+            {
+                var energy1 = Energy(input, (new Coord(-1, j), Dir.S));
+                var energy2 = Energy(input, (new Coord(input.Count, j), Dir.N));
+
+                result = Math.Max(result, Math.Max(energy1, energy2));
+            }
+
+            Console.WriteLine(result);
+        }
+
+        private static int Energy(List<string> input, (Coord, Dir) start)
+        {
             var work = new Stack<(Coord Pos, Dir Dir)>();
-            work.Push((new Coord(0, -1), Dir.E));
+            work.Push(start);
 
             var history = new HashSet<(Coord Pos, Dir Dir)>();
+            var result = new HashSet<Coord>();
 
             while (work.TryPop(out var current))
             {
                 if (history.Contains(current))
                 {
-                    Console.WriteLine($"Cycle at {current.Pos}, {current.Dir}");
                     continue;
                 }
 
@@ -30,9 +54,10 @@ namespace AdventOfCode2023
                 var next = current.Pos.Move(current.Dir);
                 if (next.X < 0 || next.Y < 0 || next.X >= input.Count || next.Y >= input[0].Length)
                 {
-                    Console.WriteLine($"Out of bounds at {next}, {current.Dir}");
                     continue;
                 }
+
+                result.Add(next);
 
                 switch ((input[next.X][next.Y], current.Dir))
                 {
@@ -82,28 +107,7 @@ namespace AdventOfCode2023
                 }
             }
 
-            var result = 0;
-            for (int i = 0; i < input.Count; i++)
-            {
-                for (int j = 0; j < input[0].Length; j++)
-                {
-                    var dirs = new [] { Dir.N, Dir.S, Dir.E, Dir.W}.Where(it => history.Contains((new Coord(i, j), it))).ToList();
-                    Console.Write(dirs.Count switch
-                    {
-                        0 => input[i][j],
-                        1 => dirs[0] switch { Dir.N => '^', Dir.S => 'v', Dir.E => '>', Dir.W => '<' },
-                        2 => '2',
-                        3 => '3',
-                        4 => '4',
-                    });
-
-                    result += dirs.Count > 0 ? 1 : 0;
-                }
-
-                Console.WriteLine();
-            }
-
-            Console.WriteLine(result);
+            return result.Count;
         }
     }
 }
