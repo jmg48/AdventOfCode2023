aab319b895580edfaec6e2493b81dfa9f5c35bea
Jon Glass <jglass@blackfog.com>
2023-12-19 09:21:44 +0000

    Day 19 Part 2

--- AdventOfCode2023/Day19.cs
+++ AdventOfCode2023/Day19.cs
@@ -11,81 +11,73 @@ namespace AdventOfCode2023
         {
             var input = File.ReadAllText("Input19.txt").Split("\n\n");
 
-            var rules = input[0].Split("\n").Select(it =>
+            var workflows = input[0].Split("\n").Select(it =>
             {
                 var match = Regex.Match(it, @"(\w+){(.*)}");
                 return (match.Groups[1].Value, match.Groups[2].Value.Split(","));
             }).ToDictionary(it => it.Item1, it => it.Item2);
 
-            var parts = input[1]
-                .Split("\n", StringSplitOptions.RemoveEmptyEntries)
-                .Select(it => it[1..^1]).Select(it =>
-                {
-                    var split = it.Split('=', ',');
-                    return new Dictionary<string, int>
-                    {
-                        {split[0], int.Parse(split[1]) },
-                        {split[2], int.Parse(split[3]) },
-                        {split[4], int.Parse(split[5]) },
-                        {split[6], int.Parse(split[7]) },
-                    };
-                })
-                .ToList();
-
-            var result = 0;
-            foreach (var part in parts)
+            long result = 0;
+            var parts = new Stack<(string, Dictionary<string, (int Low, int High)>)>();
+            parts.Push(("in",new Dictionary<string, (int, int)>
             {
-                var done = false;
-                var rule = rules["in"];
-                while (!done)
+                {"x", (1, 4000)},
+                {"m", (1, 4000)},
+                {"a", (1, 4000)},
+                {"s", (1, 4000)},
+            }));
+
+            while (parts.TryPop(out var it))
+            {
+                var (ruleKey, part) = it;
+
+                if (part.Values.Any(it => it.High < it.Low))
                 {
-                    foreach (var rulePart in rule)
+                    // If the part range has no legal values for one of the categories then we're done with it
+                    continue;
+                }
+
+                switch (ruleKey)
+                {
+                    // "A" is a special rule key meaning we Accept the part range and add its combinations to the result
+                    case "A":
+                        result += part.Values
+                            .Select(it => Math.Max(0, it.High - it.Low + 1))
+                            .Aggregate((long)1, (a, b) => a * b);
+                        continue;
+                    // "R" is a special rule key meaning we Reject the part range
+                    case "R":
+                        continue;
+                }
+
+                // Otherwise we process the rules in the workflow associated with the rule key
+                var workflow = workflows[ruleKey];
+                foreach (var rule in workflow)
+                {
+                    if (!rule.Contains(":"))
                     {
-                        if (rulePart.Contains(":"))
-                        {
-                            var split = Regex.Match(rulePart, @"(\w+)(<|>)(\d+):(\w+)");
-                            var l = part[split.Groups[1].Value];
-                            var op = split.Groups[2].Value;
-                            var r = int.Parse(split.Groups[3].Value);
-                            var d = split.Groups[4].Value;
-                            var test = op switch { "<" => l < r, ">" => l > r };
-                            if (test)
-                            {
-                                if (d == "A")
-                                {
-                                    result += part.Values.Sum();
-                                    done = true;
-                                    break;
-                                }
-                                else if (d == "R")
-                                {
-                                    done = true;
-                                    break;
-                                }
-                                else
-                                {
-                                    rule = rules[d];
-                                    break;
-                                }
-                            }
-                        }
-                        else if (rulePart == "A")
-                        {
-                            result += part.Values.Sum();
-                            done = true;
-                            break;
-                        }
-                        else if (rulePart == "R")
-                        {
-                            done = true;
-                            break;
-                        }
-                        else
-                        {
-                            rule = rules[rulePart];
-                            break;
-                        }
+                        // If the rule doesn't have a condition then we just "goto" the workflow specified by the rule
+                        parts.Push((rule, part));
+                        break;
                     }
+
+                    var split = Regex.Match(rule, @"(\w+)(<|>)(\d+):(\w+)");
+                    var op = split.Groups[2].Value;
+                    var r = int.Parse(split.Groups[3].Value);
+                    var d = split.Groups[4].Value;
+
+                    // We split the incoming part range into a 
+                    var lo = part.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    var hi = part.ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+                    lo[split.Groups[1].Value] = (lo[split.Groups[1].Value].Low, op switch { "<" => r - 1, ">" => r });
+                    hi[split.Groups[1].Value] = (op switch { "<" => r, ">" => r + 1 }, hi[split.Groups[1].Value].High);
+                    
+                    parts.Push((d, op switch { "<" => lo, ">" => hi }));
+                    part = op switch { "<" => hi, ">" => lo };
+
+                    Console.WriteLine(string.Join(",", lo.Select(kvp => $"{kvp.Key}={kvp.Value}")));
+                    Console.WriteLine(string.Join(",", hi.Select(kvp => $"{kvp.Key}={kvp.Value}")));
+
                 }
             }
 
