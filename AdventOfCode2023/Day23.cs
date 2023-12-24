a76c928ee94de6c259a338892945360f0876822d
Jon Glass <jglass@blackfog.com>
2023-12-24 19:58:17 +0000

    Revert dodgy parallel looping - Part 2 v.v. slow now!

--- AdventOfCode2023/Day23.cs
+++ AdventOfCode2023/Day23.cs
@@ -19,130 +19,119 @@ namespace AdventOfCode2023
             var start = new Coord(0, 1);
             var dest = new Coord(input.Count - 1, input[0].Length - 2);
 
-            var forks = new ConcurrentStack<((Coord, int), HashSet<Coord>)>();
+            var forks = new Stack<((Coord, int), HashSet<Coord>)>();
             forks.Push(((start, 0), new HashSet<Coord>()));
             while (forks.TryPop(out var popped))
             {
-                var work = new List<((Coord, int), HashSet<Coord>)> { popped };
-                for (var i = 0; i < 256; i++)
+                var ((pos, dist), visited) = popped;
+                while (true)
                 {
-                    if (forks.TryPop(out var popped2))
-                    {
-                        work.Add(popped2);
-                    }
-                }
+                    visited.Add(pos);
 
-                var x = work.AsParallel().WithDegreeOfParallelism(32).Select(it =>
-                {
-                    var ((pos, dist), visited) = it;
-                    while (true)
+                    if (pos == dest)
                     {
-                        visited.Add(pos);
-
-                        if (pos == dest)
+                        Console.WriteLine($"Reached dest with path {dist}");
+                        if (dist > result)
                         {
-                            Console.WriteLine($"Reached dest with path {dist}");
-                            return dist;
+                            result = dist;
                         }
 
-                        var nexts = new List<(Coord, int)>();
-                        foreach (var dir in new[] { Dir.N, Dir.S, Dir.E, Dir.W })
+                        break;
+                    }
+
+                    var nexts = new List<(Coord, int)>();
+                    foreach (var dir in new[] { Dir.N, Dir.S, Dir.E, Dir.W })
+                    {
+                        var next = pos.Move(dir);
+                        if (next.X > 0 && next.Y >= 0 && next.X < input.Count && next.Y < input[0].Length &&
+                            !visited.Contains(next))
                         {
-                            var next = pos.Move(dir);
-                            if (next.X > 0 && next.Y >= 0 && next.X < input.Count && next.Y < input[0].Length && !visited.Contains(next))
+                            if (part == 1)
                             {
-                                if (part == 1)
+                                switch (input[(int)next.X][(int)next.Y])
                                 {
-                                    switch (input[(int)next.X][(int)next.Y])
-                                    {
-                                        case '#':
-                                            break;
-                                        case '.':
-                                            nexts.Add((next, dist + 1));
-                                            break;
-                                        case '>':
-                                            next = next.Move(Dir.E);
-                                            if (next != pos)
-                                            {
-                                                nexts.Add((next, dist + 2));
-                                            }
+                                    case '#':
+                                        break;
+                                    case '.':
+                                        nexts.Add((next, dist + 1));
+                                        break;
+                                    case '>':
+                                        next = next.Move(Dir.E);
+                                        if (next != pos)
+                                        {
+                                            nexts.Add((next, dist + 2));
+                                        }
 
-                                            break;
-                                        case '^':
-                                            next = next.Move(Dir.N);
-                                            if (next != pos)
-                                            {
-                                                nexts.Add((next, dist + 2));
-                                            }
+                                        break;
+                                    case '^':
+                                        next = next.Move(Dir.N);
+                                        if (next != pos)
+                                        {
+                                            nexts.Add((next, dist + 2));
+                                        }
 
-                                            break;
-                                        case '<':
-                                            next = next.Move(Dir.W);
-                                            if (next != pos)
-                                            {
-                                                nexts.Add((next, dist + 2));
-                                            }
+                                        break;
+                                    case '<':
+                                        next = next.Move(Dir.W);
+                                        if (next != pos)
+                                        {
+                                            nexts.Add((next, dist + 2));
+                                        }
 
-                                            break;
-                                        case 'v':
-                                            next = next.Move(Dir.S);
-                                            if (next != pos)
-                                            {
-                                                nexts.Add((next, dist + 2));
-                                            }
+                                        break;
+                                    case 'v':
+                                        next = next.Move(Dir.S);
+                                        if (next != pos)
+                                        {
+                                            nexts.Add((next, dist + 2));
+                                        }
 
-                                            break;
-                                        default:
-                                            throw new Exception();
-                                    }
+                                        break;
+                                    default:
+                                        throw new Exception();
                                 }
-                                else
+                            }
+                            else
+                            {
+                                switch (input[(int)next.X][(int)next.Y])
                                 {
-                                    switch (input[(int)next.X][(int)next.Y])
-                                    {
-                                        case '#':
-                                            break;
-                                        case '.':
-                                        case '>':
-                                        case '^':
-                                        case '<':
-                                        case 'v':
-                                            nexts.Add((next, dist + 1));
-                                            break;
-                                        default:
-                                            throw new Exception();
-                                    }
+                                    case '#':
+                                        break;
+                                    case '.':
+                                    case '>':
+                                    case '^':
+                                    case '<':
+                                    case 'v':
+                                        nexts.Add((next, dist + 1));
+                                        break;
+                                    default:
+                                        throw new Exception();
                                 }
                             }
                         }
-
-                        if (nexts.Count == 0)
-                        {
-                            Console.WriteLine($"Dead end at {pos} with path length {dist}");
-                            return -1;
-                        }
-
-                        switch (nexts.Count)
-                        {
-                            case 1:
-                                (pos, dist) = nexts[0];
-                                break;
-                            case 2 or 3 or 4:
-                                //Console.WriteLine($"Fork at {pos} with path length {dist}");
-                                (pos, dist) = nexts[0];
-                                for (int i = 1; i < nexts.Count; i++)
-                                {
-                                    forks.Push((nexts[i], new HashSet<Coord>(visited)));
-                                }
-
-                                break;
-                        }
                     }
-                }).Max();
-                if (x > result)
-                {
-                    result = x;
-                    File.AppendAllLines("C:\\src\\23.txt", new[] { result.ToString() });
+
+                    if (nexts.Count == 0)
+                    {
+                        Console.WriteLine($"Dead end at {pos} with path length {dist}");
+                        break;
+                    }
+
+                    switch (nexts.Count)
+                    {
+                        case 1:
+                            (pos, dist) = nexts[0];
+                            break;
+                        case 2 or 3 or 4:
+                            //Console.WriteLine($"Fork at {pos} with path length {dist}");
+                            (pos, dist) = nexts[0];
+                            for (int i = 1; i < nexts.Count; i++)
+                            {
+                                forks.Push((nexts[i], new HashSet<Coord>(visited)));
+                            }
+
+                            break;
+                    }
                 }
             }
 
